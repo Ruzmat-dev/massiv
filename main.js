5622a84947f95969e3ad57c559cbceb6bf328376
Universal0090 <idealxorezm@gmail.com>
2022-10-31 08:38:54 -0900

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+
+
+// masala 1 
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+
+// for ( i = 1 ; i <= n ; i++) {
+//      a.push(i)
+// }
+// console.log(a);
+
+
+
+// masala 2 
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+
+// for ( i = 1 ; i <= n ; i++) {
+//     a.push(Math.pow(2,i))
+// } 
+// console.log(a);
+
+
+
+// masala 3 
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+// let sum = 0 ;
+
+// for ( i = 1 ; i <= n ; i++) {
+//     sum = sum + i 
+//     a.push(i)
+//     console.log( 'massiv =  [' + a+ ']');
+// } 
+// console.log( 'yigindisi =  ' + sum);
+
+
+
+
+// masala 4
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+// let toq = 0 ;
+// let juft = 0 ;
+
+// for ( i = 1 ; i <= 10 ; i++) {
+//     a.push(i*10) 
+//     if( i % 2 == 1) {
+//         toq = toq + i    
+//     }else {
+//         console.log();
+//         juft = juft - 1 
+//     }
+// } 
+// console.log('index Toqlar yigindisi =  ' + toq);
+// console.log('index Juftlar ayirmasi =  ' + juft );
+// console.log('Massiv =  ' + a );
+
+
+
+// masala 5
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+// let sum = 0 ;
+
+// for ( i = 1 ; i <= n ; i++) {
+//     a.push(i)
+//     sum = sum + i
+// } 
+// console.log('Massiv =  ' + a );
+// console.log('Yigindisi =  ' + sum );
+
+
+
+
+// masala 6
+// let n = +prompt( ' N soni kiritig: ' );
+// let a = [] ;
+// let b = 0 ;
+
+// for ( i = 1 ; i <= n ; i++) {
+//     b = n - i
+//     a.push(b)
+// } 
+// console.log('Massiv =  ' + a );
+
+
+
+
+
+
+// masala 7
+// let a_ism = +prompt( ' Ismingiz kim ? ' );
+// let a_yosh = +prompt( ' Yoshingiz nechida ? ' );
+// let b_ism = +prompt( ' Ismingiz kim ? ' );
+// let b_yosh = +prompt( ' Yoshingiz nechida ? ' );
+// let c_ism = +prompt( ' Ismingiz kim ? ' );
+// let c_yosh = +prompt( ' Yoshingiz nechida ? ' );
+
+// let a_modul = [a_ism , a_yosh] ;
+// let b_modul = [b_ism , b_yosh] ;
+// let c_modul = [c_ism , c_yosh] ;
+
+// let yoshlar_farqi = a_modul[1] - b_modul[1] - c_modul[1];
+// console.log(' Yoshlari farqi =  '  + yoshlar_farqi );
+
